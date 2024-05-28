7d5d20e93a58680a814c4a017dedcd794472973a
Hugo Christian Pereira Gomes <hugo@n2y.com.br>
2024-05-27 22:44:03 -0300

    V0.0.7e
    Teste para alteração visual da página Jobs.

--- js/scripts.js
+++ js/scripts.js
@@ -17,13 +17,13 @@ $(function(){
     $("#space").attr('style', 'display: none;');
 
     /** Alterar formatação para dar espaçamentos */
-    $("#hidraulica").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0; color: #FFF');
+    $("#hidraulica").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0;');
     $("#eletrica").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0 2rem 0.75rem;');
-    $("#pintura_predial").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0; color: #FFF');
+    $("#pintura_predial").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0;');
     $("#revestimentos").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0 2rem 0.75rem;');
-    $("#impermeabilizacao").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0; color: #FFF');
+    $("#impermeabilizacao").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0;');
     $("#pintura_interna").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0 2rem 0.75rem;');
-    $("#telhadista").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0; color: #FFF');
+    $("#telhadista").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding: 2rem 0.75rem 2rem 0;');
   }else{
     /** Remover blocos desnecessários */
     $("#eletrica").attr('style', 'display: none;');
@@ -31,13 +31,13 @@ $(function(){
     $("#pintura_interna").attr('style', 'display: none;');
 
     /** Alterar formatação para dar espaçamentos */
-    $("#hidraulica").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem; color: #FFF');
+    $("#hidraulica").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem;');
     $("#eletrica2").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem;');
-    $("#pintura_predial").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem; color: #FFF');
+    $("#pintura_predial").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem;');
     $("#revestimentos2").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem;');
-    $("#impermeabilizacao").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem; color: #FFF');
+    $("#impermeabilizacao").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem;');
     $("#pintura_interna2").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem;');
-    $("#telhadista").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem; color: #FFF');
+    $("#telhadista").attr('style', 'margin-top: 2rem; margin-bottom: 2rem; padding-top: 2rem;');
     $("h4.col-12.text-center").attr('style', 'margin-top: 1rem');
 
   }
